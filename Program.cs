547ed6d369c503f3b1771e1e20b35eecb8bff583
Eldiana <129501744+Elldiana@users.noreply.github.com>
2025-01-28 14:05:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FaktorielBul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            long sonuc = faktorielBul(sayi);
+            Console.WriteLine($"{sayi}!={sonuc}");
+            Console.ReadKey();
+
+        }
+        public static long faktorielBul(int n)
+        {
+            long faktoriel = 1;
+            for(int a = 1; a<=n; a++)
+            {
+                faktoriel *= a;
+            }
+            return faktoriel;
+        }
+    }
+}
